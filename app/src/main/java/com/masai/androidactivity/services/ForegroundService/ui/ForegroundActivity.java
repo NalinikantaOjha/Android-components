be3092bd047731bd36e430d43c65db93a7b37a33
nalin <nalinikanta7978@gmail.com>
2022-01-15 15:40:23 +0530

    Services

---
+++ app/src/main/java/com/masai/androidactivity/services/ForegroundService/ui/ForegroundActivity.java
@@ -0,0 +1,16 @@
+package com.masai.androidactivity.services.ForegroundService.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.masai.androidactivity.R;
+
+public class ForegroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foreground);
+    }
+}
---
+++ app/src/main/java/com/masai/androidactivity/services/ServiceActivity.java
@@ -0,0 +1,16 @@
+package com.masai.androidactivity.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.masai.androidactivity.R;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+    }
+}
